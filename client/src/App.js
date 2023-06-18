11c78d14cd3dd51f06e61b419e307e40d04b444e
Ngoc Doan <ndoan@soka.edu>
2023-06-18 22:15:10 +0700

    styling markers done, infowindow not done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import MapContainer from './Map/Map';
 import './App.css';
+import ColorBar from './Map/data/ColorBar'
 
 function App() {
   return (
     <div className="App">
+      <ColorBar />
       <MapContainer />
     </div>
   );
